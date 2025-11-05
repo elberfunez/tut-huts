3395261c3d42085c68176a2c41ddbc8c3137142e
Elber Funez <elberfunez@gmail.com>
2025-11-05 17:36:21 -0500

    change phone number

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 export const RESERVATION_URL: string = 'https://www.campspot.com/book/tuthutsrvpark';
 export const PARK_ADDRESS: string = '12465 Tutwiler Rd, Parrish, AL 35580';
-export const PARK_PHONE_NUMBER: string = '(202) 530-0408';
+export const PARK_PHONE_NUMBER: string = '(205) 530-0408';
 export const PARK_EMAIL: string = 'tuthutsrvpark@gmail.com';
 export const FACEBOOK_URL_LINK: string = 'https://www.facebook.com/tuthuts/';
 export const WEB3FORMS_ACCESSKEY: string = '36d12330-1fcb-4fa4-a19f-8bf2b958a69a';
